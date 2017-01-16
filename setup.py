ef3c9f934706997189309cc129f0a7ed8935deb0
Jacob Senecal <jacob.senecal@gmail.com>
2017-01-16 12:29:04 -0600

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyhatchbuck',
+    version='0.1dev',
+    description='Python library for Hatchbuck API',
+    url='https://github.com/jakesen/pyhatchbuck',
+    author='Jacob Senecal',
+    author_email='jacob.senecal@gmail.com',
+    license='MIT',
+    packages=['pyhatchbuck',],
+)
