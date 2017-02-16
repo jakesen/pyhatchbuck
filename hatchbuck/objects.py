0e95ae87b976d653742c75b5669cd7ef93b4c931
Jacob Senecal <jacob.senecal@gmail.com>
2017-02-16 13:38:54 -0600

    Corrections and additions to contact object

--- hatchbuck/objects.py
+++ hatchbuck/objects.py
@@ -55,15 +55,16 @@ class User(ApiObject):
 
 class Contact(ApiObject):
     contactId = ApiStringAttribute()
+    sourceId = ApiStringAttribute()
     firstName = ApiStringAttribute()
     lastName = ApiStringAttribute()
     title = ApiStringAttribute()
     company = ApiStringAttribute()
     salesRep = User()
     emails = ApiObjectList(Email)
-    phone = ApiObjectList(Phone)
+    phones = ApiObjectList(Phone)
     tags = ApiObjectList(Tag)
-    campaign = ApiObjectList(Campaign)
+    campaigns = ApiObjectList(Campaign)
     status = Status()
     temperature = Temperature()
     addresses = ApiObjectList(Address)
@@ -76,11 +77,60 @@ class Contact(ApiObject):
     referredBy = ApiStringAttribute()
 
     def __init__(self, api_key, json_data=None):
+        # make sure lists are instance attributes
+        self.emails = ApiObjectList(Email)
+        self.phones = ApiObjectList(Phone)
+        self.tags = ApiObjectList(Tag)
+        self.campaigns = ApiObjectList(Campaign)
+        self.addresses = ApiObjectList(Address)
+        self.socialNetworks = ApiObjectList(SocialNetwork)
+        self.instantMessaging = ApiObjectList(InstantMessage)
+        self.website = ApiListAttribute()
+        self.customFields = ApiObjectList(CustomField)
+        # call super to load json values
         super(Contact, self).__init__(json_data)
+        # cache api key to use in save method
         self.api_key = api_key
+
+    def add_email(self, address, type):
+        self.emails.append(Email({'address': address, 'type': type}))
+
+    def set_status(self, name):
+        self.status = Status({'name': name})
+
+    def add_address(self, street, city, state, zip, type):
+        self.addresses.append(Address({
+            'street': street,
+            'city': city,
+            'state': state,
+            'zip': zip,
+            'type': type
+        }))
+
+    def set_temperature(self, id):
+        self.temperature = Temperature({'id': id})
+
+    def add_phone(self, number, type):
+        self.phones.append(Phone({
+            'number': number,
+            'type': type
+        }))
+
+    def add_social_network(self, address, type):
+        self.socialNetworks.append(SocialNetwork({'address': address, 'type': type}))
+
+    def add_instant_message_address(self, address, type):
+        self.instantMessaging.append(InstantMessage({'address': address, 'type': type}))
+
+    def add_website(self, websiteUrl):
+        self.website.append({'websiteUrl': websiteUrl})
 
     def save(self):
         from hatchbuck.api import HatchbuckAPI
         if self.contactId == "":
-            self.load_dict(HatchbuckAPI(self.api_key).add_contact(self))
-            return self.contactId != ""
+            new_data = HatchbuckAPI(self.api_key).add_contact(self)
+            if new_data != None:
+                self.load_dict(new_data)
+                return self.contactId != ""
+            else:
+                return False
