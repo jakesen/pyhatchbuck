4c51a2c05a43f1af6d21ddb4cd0147f27aa5105b
Jacob Senecal <jacob.senecal@gmail.com>
2017-02-18 10:07:46 -0600

    Test for updating contact email

--- tests/test_update_contact.py
+++ tests/test_update_contact.py
@@ -5,7 +5,7 @@ import unittest
 from hatchbuck.api import HatchbuckAPI
 from hatchbuck.objects import Contact
 
-class TestAddContact(unittest.TestCase):
+class TestUpdateContact(unittest.TestCase):
 
     def setUp(self):
         # Fake key can be used with existing cassettes
@@ -27,6 +27,22 @@ class TestAddContact(unittest.TestCase):
         self.assertEqual(success, True)
         self.assertEqual(contact.firstName, "Jill")
         self.assertEqual(contact.lastName, "Smith")
+
+    @vcr.use_cassette(
+        'tests/fixtures/cassettes/test_update_contact_email.yml',
+        filter_query_parameters=['api_key']
+    )
+    def test_update_contact_email(self):
+        hatchbuck = HatchbuckAPI(self.test_api_key)
+        contact = hatchbuck.new_contact()
+        contact_id = "d1F4Tm1tcUxVRmdFQmVIT3lhVjNpaUtxamprakk5S3JIUGRmVWtHUXJaRTE1"
+        contact = hatchbuck.search_contacts(contactId=contact_id)[0]
+        self.assertEqual(contact.emails[0].address, "jill@pyhatchbuck.net")
+        contact.emails[0].address = "jill.smith@pyhatchbuck.net"
+        success = contact.save()
+        self.assertEqual(success, True)
+        self.assertEqual(contact.emails[0].address, "jill.smith@pyhatchbuck.net")
+
 
 if __name__ == '__main__':
     unittest.main()
