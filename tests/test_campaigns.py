8fa0dca5cd5187126a10197883348fc6b16544b5
Jacob Senecal <jacob.senecal@gmail.com>
2017-03-02 08:56:52 -0600

    Test get campaigns by email

--- tests/test_campaigns.py
+++ tests/test_campaigns.py
@@ -22,6 +22,19 @@ class TestCampaigns(unittest.TestCase):
         self.assertEqual(contact.contactId, contact_id)
         campaigns = contact.get_campaigns()
         self.assertEqual(campaigns[0].name, "Brochure Request Followup")
+        self.assertEqual(campaigns[0].step, 0)
+        self.assertEqual(campaigns[0].id, "b1BFUnM1Unh0MDVVOVJEWUc1d0pTM0pUSVY4QS0xOW5GRHRsS05DXzNXazE1")
+
+    @vcr.use_cassette(
+        'tests/fixtures/cassettes/test_get_contact_campaigns_by_email.yml',
+        filter_query_parameters=['api_key']
+    )
+    def test_get_contact_campaigns_by_email(self):
+        hatchbuck = HatchbuckAPI(self.test_api_key)
+        contact_email = "jill.smith@pyhatchbuck.net"
+        campaigns = hatchbuck.get_campaigns(contact_email)
+        self.assertEqual(campaigns[0].name, "Brochure Request Followup")
+        self.assertEqual(campaigns[0].step, 0)
         self.assertEqual(campaigns[0].id, "b1BFUnM1Unh0MDVVOVJEWUc1d0pTM0pUSVY4QS0xOW5GRHRsS05DXzNXazE1")
 
 if __name__ == '__main__':
