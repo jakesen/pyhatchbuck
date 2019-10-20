50dbad3e5b9b76f3c52a84daf9cd14fcb88ab6c2
CristhianMotoche <cristhian.motoche@gmail.com>
2019-10-20 03:21:51 +0000

    Add test when there are no customFields

--- tests/test_search_contact_custom_fields.py
+++ tests/test_search_contact_custom_fields.py
@@ -3,7 +3,9 @@ import vcr
 import unittest
 
 from hatchbuck.api import HatchbuckAPI
-from hatchbuck.objects import Contact
+
+
+CONTACT_ID = "d1F4Tm1tcUxVRmdFQmVIT3lhVjNpaUtxamprakk5S3JIUGRmVWtHUXJaRTE1"
 
 
 class TestCustomFields(unittest.TestCase):
@@ -12,16 +14,26 @@ class TestCustomFields(unittest.TestCase):
         self.test_api_key = os.environ.get("HATCHBUCK_API_KEY", "ABC123")
 
     @vcr.use_cassette(
-        'tests/fixtures/cassettes/test_search_contact_with_customFields.yml',
-        filter_query_parameters=['api_key']
+      'tests/fixtures/cassettes/test_search_contact_with_customFields.yml',
+      filter_query_parameters=['api_key']
     )
     def test_search_contact_with_customFields(self):
         hatchbuck = HatchbuckAPI(self.test_api_key)
-        contact_id = "d1F4Tm1tcUxVRmdFQmVIT3lhVjNpaUtxamprakk5S3JIUGRmVWtHUXJaRTE1"
-        contact = hatchbuck.search_contacts(contactId=contact_id)[0]
-        self.assertEqual(contact.contactId, contact_id)
+        contact = hatchbuck.search_contacts(contactId=CONTACT_ID)[0]
+        self.assertEqual(contact.contactId, CONTACT_ID)
         customFields = contact.customFields
         self.assertEqual(len(customFields), 2)
         self.assertEqual(customFields[0].name, "Custom1")
         self.assertEqual(customFields[0].type, "Integer")
         self.assertEqual(customFields[0].value, "42")
+
+    @vcr.use_cassette(
+      'tests/fixtures/cassettes/test_search_contact_without_customFields.yml',
+      filter_query_parameters=['api_key']
+    )
+    def test_search_contact_without_customFields(self):
+        hatchbuck = HatchbuckAPI(self.test_api_key)
+        contact = hatchbuck.search_contacts(contactId=CONTACT_ID)[0]
+        self.assertEqual(contact.contactId, CONTACT_ID)
+        customFields = contact.customFields
+        self.assertEqual(len(customFields), 0)
