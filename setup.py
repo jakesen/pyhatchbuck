e58fdcc7eb8ef1c7a7f2e80a312e8c65d3166fa1
Jacob Senecal <jacob.senecal@gmail.com>
2019-12-12 15:12:48 -0600

    Update pyhatchbuck version to 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyhatchbuck',
-    version='0.2',
+    version='0.3',
     description='Python library for Hatchbuck API',
     url='https://github.com/jakesen/pyhatchbuck',
     author='Jacob Senecal',
